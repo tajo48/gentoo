a25baf05051804b5d0bcb8e3bb63359a92010b58
tajo48 <tajo161626@gmail.com>
2021-12-18 20:50:32 +0100

    update

--- files/github.sh
+++ files/github.sh
@@ -3,6 +3,10 @@ git config --global credential.helper store
 git config --global user.email 'tajo161626@gmail.com'
 git config --global user.name 'tajo48'
 
+if test -f "/home/tajo48/.ssh/id_rsa.pub"; then
+    rm -rf /home/tajo48/.ssh
+fi
+
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -b 4096 -C “tajo161626@gmail.com” -q -N "" -P ""
 cat /home/tajo48/.ssh/id_rsa.pub | xclip
 echo "paste ssh key to github" | dmenu
