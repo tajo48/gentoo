7668292b06c40ece9014f14a9500e632f6337721
tajo48 <tajo161626@gmail.com>
2021-12-18 21:19:07 +0100

    update

--- builds/dwm/config.h
+++ builds/dwm/config.h
@@ -64,7 +64,7 @@ static const char *sscmd[]  = { "sh", "/home/tajo48/ARCH/files/ss.sh", NULL };
 static const char *offcmd[]  = { "sh", "/home/tajo48/ARCH/files/off.sh", NULL };
 static const char *updatecmd[]  = { "sh", "/home/tajo48/ARCH/files/upgradefiles.sh", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x24", "-e", "tmux", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "100x24", "-e", "tmux", NULL };
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
