fed937e5a2bc05dc569c82a6b9de4a9a12f0b5e5
tajo48 <55502906+tajo48@users.noreply.github.com>
2021-12-24 19:58:17 +0100

    Update upgradefiles.sh

--- files/upgradefiles.sh
+++ files/upgradefiles.sh
@@ -4,7 +4,7 @@
 ping -q -w 1 -c 1 google.com > /dev/null && echo "internet ok" || exit
 
 sudo rm -r ~/ARCH
-git clone https://github.com/tajo48/ARCH.git | dmenu
+git clone git@github.com:tajo48/ARCH.git | dmenu
 
 rm -rf ~/.xinitrc
 cp ~/ARCH/files/xinitrc ~/.xinitrc
