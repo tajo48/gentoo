77d6478992e684f836a4af5c2d443810853e5c58
tajo48 <tajo161626@gmail.com>
2022-01-01 23:11:05 +0100

    update

--- files/wallpaper.sh
+++ files/wallpaper.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 cd ~/
-#[ "$(ls -A ~/Wallpapers)" ] && echo "OK" || exit
+
+
+if [ "$(ls -A ~/Wallpapers)" ]
+then
+
 moviename=$(ls ~/Wallpapers | shuf -n 1)
 moviedir=$(pwd)"/Wallpapers/"$moviename
 movietime=$(ffprobe -i $moviedir -show_format -v quiet | sed -n 's/duration=//p' | sed 's/\..*$//')
 movierandom=$(shuf -i 1-$movietime -n 1)
 ffmpeg -y -ss $movierandom -i $moviedir -vframes 1 -q:v 2 ~/ARCH/photos/wallpaper.jpg
+
+else
+	echo $(pwn)"Wallpapers/ dir is empty" 
+fi
 
