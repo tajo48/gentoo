59f8c3c08ecd286821908bd0ee6bdadcaf45e74d
tajo48 <tajo161626@gmail.com>
2022-01-01 21:07:49 +0100

    update

--- files/wallpaper.sh
+++ files/wallpaper.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-moviedir=${1}
+#moviedir=file to movie}
+moviedir=${1} 
 movietime=$(ffprobe -i $moviedir -show_format -v quiet | sed -n 's/duration=//p' | sed 's/\..*$//')
-echo "Film time: " $movietime
 movierandom=$(shuf -i 1-$movietime -n 1)
-echo "Movie random frame:" $movierandom
 ffmpeg -y -ss $movierandom -i $moviedir -vframes 1 -q:v 2 ~/ARCH/photos/wallpaper.jpg
 
