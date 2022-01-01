db5dc519268a91ef9a589a24018051eba265f6a8
tajo48 <tajo161626@gmail.com>
2022-01-02 00:51:40 +0100

    update

--- files/wallpaper.sh
+++ files/wallpaper.sh
@@ -8,9 +8,13 @@ then
 moviename=$(ls ~/Wallpapers | shuf -n 1)
 moviedir=$(pwd)"/Wallpapers/"$moviename
 movietime=$(ffprobe -i $moviedir -show_format -v quiet | sed -n 's/duration=//p' | sed 's/\..*$//')
+movietime=$(calc $movietime - 1)
+movietime=$(echo $movietime | sed 's/ //g')
 movierandom=$(shuf -i 1-$movietime -n 1)
-ffmpeg -y -ss $movierandom -i $moviedir -vframes 1 -q:v 2 ~/ARCH/photos/wallpaper.jpg
+moviesecdot=$(shuf -i 0-9999 -n 1)
 
+ffmpeg -y -ss $movierandom.$moviesecdot -i $moviedir -vframes 1 -q:v 2 ~/ARCH/photos/wallpaper.jpg
+echo $movietime.$moviesecdot
 else
 	echo $(pwd)"/Wallpapers/ dir is empty" 
 fi
