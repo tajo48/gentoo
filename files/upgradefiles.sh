7e9c6d3757a78463cbadee97530926f002d6bc5d
tajo48 <55502906+tajo48@users.noreply.github.com>
2022-01-02 21:19:00 +0100

    Update upgradefiles.sh

--- files/upgradefiles.sh
+++ files/upgradefiles.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd ~/
 
+
 ping -q -w 1 -c 1 google.com > /dev/null && echo "internet ok" || exit
+ssh-keygen -F github.com || ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 sudo rm -r ~/ARCH
 git clone git@github.com:tajo48/ARCH.git | dmenu
