d070b8e44c88776fe7004e672631f91d83de70cd
tajo48 <tajo161626@gmail.com>
2022-01-04 00:35:52 +0100

    update

--- after.sh
+++ after.sh
@@ -1,16 +1,7 @@
 #! /bin/bash
-sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Syy
 
-#programs
-pacman -S --noconfirm alsa-utils netctl mtools dialog wpa_supplicant dhcpcd git neofetch mc xclip clipmenu rtorrent openssh tmux arandr slock
-pacman -S --noconfirm obs-studio blender bashtop calc virtualbox virtualbox-host-modules-arch vlc youtube-dl htop tar p7zip xcompmgr audacity gimp cmus discord nautilus
-pacman -S --noconfirm make alsa-firmware wget xorg xorg-server xorg-xinit curl libxft fakeroot binutils patch pkgconf base-devel python-pywal alsa
-pacman -S --noconfirm firefox zsh feh virtualbox-guest-utils python-pynvim nodejs yarn torbrowser-launcher
-pacman -S --noconfirm xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau xf86-video-fbdev
-pacman -S --noconfirm ttf-joypixels ttf-jetbrains-mono ttf-liberation grub efibootmgr
-pacman -S --noconfirm pulseaudio linux-headers dkms ranger lm_sensors npm mpv qbittorrent
-
+#programs variable
+programs="pulseaudio linux-headers dkms iwd ranger lm_sensors npm mpv qbittorrent ttf-joypixels ttf-jetbrains-mono ttf-liberation grub efibootmgr xf86-video-vesa xf86-video-ati xf86-video-intel xf86-video-amdgpu xf86-video-nouveau xf86-video-fbdev firefox zsh feh virtualbox-guest-utils python-pynvim nodejs yarn torbrowser-launcher alsa-utils netctl mtools dialog wpa_supplicant dhcpcd git neofetch mc xclip clipmenu rtorrent openssh tmux arandr obs-studio blender bashtop calc virtualbox virtualbox-host-modules-arch vlc youtube-dl htop tar p7zip xcompmgr audacity gimp cmus discord nautilus slock make alsa-firmware wget xorg xorg-server xorg-xinit curl libxft fakeroot binutils patch pkgconf base-devel python-pywal alsa"
 
 #remake dwm st dmenu
 #remove xcompmgr
@@ -23,58 +14,101 @@ pacman -S --noconfirm pulseaudio linux-headers dkms ranger lm_sensors npm mpv qb
 #xone and soundux
 #torrent client
 #portable arch and arch install media
-#better zsh config 
+#better zsh config
 #better nvim config
 #split updatefiles.sh to updatefiles.sh and setup.sh
 #ue5 works on dwm?
 #wacom script
 #wifi dmenu
 #sudo pacman -S pavucontrol
-#Xclip notflix 
-#Notflix download or stream 
-#Cut WiFi menu 
-#Make arch usb 
+#Xclip notflix
+#Notflix download or stream
+#Cut WiFi menu
+#Make arch usb
 #Delete dmenu notify
 #Delete all terminal pref script from ctrl+win+p
 #ssh loockup (saftey measure)
 
 
-# Set date time
+
+#enable multilib
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+
+#update list of packages
+pacman -Syy
+
+#pacman install from programs variable
+pacman -S --noconfirm $programs
+
+#set date time
 ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 hwclock --systohc
 
-# Set locale to en_US.UTF-8 UTF-8
-echo "en_US.UTF-8 UTF-8
-pl_PL.UTF-8 UTF-8" >> /etc/locale.gen
+#set locale to en_US.UTF-8 UTF-8
+echo -e "en_US.UTF-8 UTF-8\npl_PL.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
-# Set hostname
-echo "ARCH" >> /etc/hostname
-echo "
-127.0.0.1	localhost
-::1		localhost
-127.0.1.1	ARCH" >> /etc/hosts
+#enable networkd and resolved
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+systemctl enable iwd.service
 
-# Set root passwd
+#network
+ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
+echo -e "[Match]\nName=en*\nName=eth*\n\n[Network]\nDHCP=yes\nIPv6PrivacyExtensions=yes\n\n[DHCPv4]\nRouteMetric=100\n\n[IPv6AcceptRA]\nRouteMetric=100" >> /etc/systemd/network/20-ethernet.network
+echo -e "[Match]\nName=wl*\n\n[Network]\nDHCP=yes\nIPv6PrivacyExtensions=yes\n\n[DHCPv4]\nRouteMetric=600\n\n[IPv6AcceptRA]\nRouteMetric=600\n" >> /etc/systemd/network/20-wlan.network
+
+#timesync
+systemctl enable systemd-timesyncd.service
+
+#set hostname
+echo "ARCH" >> /etc/hostname
+
+#set localhost
+echo -e"\n127.0.0.1	localhost\n::1		localhost\n127.0.1.1	ARCH\n" >> /etc/hosts
+
+#set root passwd
 echo -en "root\nroot" | passwd
 
-# Useradd,internet and sudo and deepin setup
-systemctl enable dhcpcd
+#internet deamon
+####################systemctl enable dhcpcd
+
+#add user
 useradd -m tajo48
+
+#set user passwd
 echo -en "root\nroot" | passwd tajo48
+
+#set user groups
 usermod -aG wheel,audio,video,optical,storage,users tajo48
+
+#set sudo password to no require
 sed -i '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^#//g' /etc/sudoers
-#sed -i '/#start-default-seat=true/a greeter-session=lightdm-deepin-greeter' /etc/lightdm/lightdm.conf
+
+#set ssh and ssh-deamon
 sed -i '/Port/s/^#//g' /etc/ssh/sshd_config
 sed -i '/Port/ s/22/2137/' /etc/ssh/sshd_config
 systemctl enable sshd
 
-grub-install --target=i386-pc ${1} 
+#journal setup
+mkdir -p /etc/systemd/journald.conf.d
+echo -e "[Journal]\nStorage=volatile\nSystemMaxUse=16M\nRuntimeMaxUse=32M\n" > /etc/systemd/journald.conf.d/10-volatile.conf
+
+#sed change realtime to noatime and atime in etc/fstab
+sudo sed -i 's/realtime/noatime/g' /etc/fstab
+sudo sed -i 's/atime/noatime/g' /etc/fstab
+
+#install grub and make config
+if  [${2} = "usb" ]
+then
+    grub-install --target=i386-pc --boot-directory /boot --removable ${1}
+    grub-install --target=x86_64-efi --efi-directory /boot --boot-directory /boot --removable
+elif [${2} = "hard" ]
+    grub-install --target=i386-pc ${1} 
+fi
 grub-mkconfig -o /boot/grub/grub.cfg
-
-
 
 
 #makepkg in root
@@ -82,46 +116,48 @@ rm /usr/bin/makepkg
 curl -k -o /usr/bin/makepkg https://raw.githubusercontent.com/tajo48/ARCH/master/files/makepkg
 chmod +x /usr/bin/makepkg
 
+#install my config repo
 cd /home/tajo48
 git clone https://github.com/tajo48/ARCH.git
 
-
+#wal for dwm,st and dmenu with zsh install
 su tajo48 << 'NO'
-
 wal -i /home/tajo48/ARCH/photos
 sh -c "$(curl -k -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 NO
 
-
 # curl -k -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    
+
 # sh -c 'curl -k -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-
-#dwm
-cd /home/tajo48
+#dotfiles
 cp /home/tajo48/ARCH/files/zshrc /home/tajo48/.zshrc
 cp /home/tajo48/ARCH/files/xinitrc /home/tajo48/.xinitrc
+
+#set zsh as default shell
 chsh -s /bin/zsh tajo48
+
+#mkdir and touch
 mkdir /home/tajo48/Downloads /home/tajo48/Desktop /home/tajo48/Pictures /home/tajo48/Git /home/tajo48/Videos /home/tajo48/Wallpapers
 touch /home/tajo48/weather
+
 #nvim
 # mkdir /home/tajo48/.config/nvim
 # cp /home/tajo48/ARCH/files/init.vim /home/tajo48/.config/nvim/init.vim
 
-
-#yay 
+#install yay
 cd /home/tajo48/Git/
 git clone https://aur.archlinux.org/yay-git.git
 cd /home/tajo48/Git/yay-git
 makepkg -s -i -c --noconfirm
+
+#install webtorrent
 cd /home/tajo48/Git/
 git clone https://aur.archlinux.org/webtorrent-cli.git
 cd /home/tajo48/Git/webtorrent-cli
 makepkg -sri --noconfirm
 
-
+#chown
 chown tajo48 -R /home/tajo48/
-
--- install.sh
+++ install.sh
@@ -1,74 +1,130 @@
 #! /bin/bash
 selectdisk(){
-		items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
-		options=()
-		IFS_ORIG=$IFS
-		IFS=$'\n'
-		for item in ${items}
-		do  
-				options+=("${item}" "")
-		done
-		IFS=$IFS_ORIG
-		result=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
-		if [ "$?" != "0" ]
-		then
-				return 1
-		fi
-		return 0    
+    items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
+    options=()
+    IFS_ORIG=$IFS
+    IFS=$'\n'
+    for item in ${items}
+    do
+        options+=("${item}" "")
+    done
+    IFS=$IFS_ORIG
+    result=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
+    if [ "$?" != "0" ]
+    then
+        return 1
+    fi
+    return 0
 }
 
 selectdisk
 disk=${result%%\ *}
-blkdiscard -f ${disk}
+usbdrive="..."
 
-#dd if=/dev/zero of=${disk}
-#dd if=/dev/random of=${disk} bs=446 count=1 
-#shred -n 0 -vz ${disk}
+for device in /sys/block/*
+do
+    if udevadm info --query=property --path=$device | grep -q ^ID_BUS=usb
+    then
+        usb=$(echo $device | sed -n 's/^.*\/block\/\(.*\)/\1/p')
+        
+        if [ ${disk} = "/dev/${usb}" ]
+        then
+            #this is usb drive
+            usbdrive=${disk}
+        fi
+        
+    fi
+done
 
-# Font
-loadkeys pl
-setfont Lat2-Terminus16.psfu.gz -m 8859-2
+if [ ${usbdrive} = ${disk} ]
+then
+    #this is usb drive
+    echo "install arch on usb drive"
+	sleep 3s
+    #wipe
+    blkdiscard -f ${disk}
+    
+    #partition
+    sgdisk -o -n 1:0:+10M -t 1:EF02 -n 2:0:+500M -t 2:EF00 -n 3:0:0 -t 3:8300 ${disk}
+    
+    #format
+    mkfs.fat -F32 ${disk}2
+    mkfs.ext4 ${disk}3
+    
+    #mount
+    mkdir -p /mnt/usb
+    mount ${disk}3 /mnt/usb
+    mkdir /mnt/usb/boot
+    mount ${disk}2 /mnt/usb/boot
 
-# else
-parted ${disk} --script mklabel msdos
-parted ${disk} --script mkpart primary linux-swap 1MiB 300MiB #boot /dev/sda1
-parted ${disk} --script mkpart primary ext4 300MiB 100% #root /dev/sda2
-parted ${disk} --script set 2 boot on
-
-# Wipefs
-wipefs ${disk}1
-wipefs ${disk}2
-
-# Mkfs
-mkfs.ext4 ${disk}2
-mkswap ${disk}1
-
-# Mount the partitions
-mount ${disk}2 /mnt
-mkdir /mnt/boot
-swapon ${disk}1
-
-# Setup the disk and partitions
-# Set up time
-timedatectl set-ntp true
-
-# Initate pacman keyring
-<<com
-pacman-key --init
-pacman-key --populate archlinux
-pacman-key --refresh-keys
-com
-
-
-# Install Arch Linux
-pacstrap /mnt base linux pacman sudo linux-firmware dosfstools wget
-
-# Generate fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Copy post-install system cinfiguration script to new /root
+	#set up time
+    timedatectl set-ntp true
+    
+    #initate pacman keyring
+    #pacman-key --init
+    #pacman-key --populate archlinux
+    #pacman-key --refresh-keys
+    
+    #pacstrap
+    pacstrap /mnt/usb base linux pacman sudo linux-firmware dosfstools wget
+    
+    #fstab
+    genfstab -U /mnt/usb > /mnt/usb/etc/fstab
+    
+    #generate fstab
+    genfstab -U /mnt >> /mnt/etc/fstab
+    
+    #copy post-install system cinfiguration script to new /root
     curl -k --create-dirs -o /mnt/root/after.sh https://raw.githubusercontent.com/tajo48/ARCH/master/after.sh
     chmod +x /mnt/root/after.sh
+    
+    #chroot into new system
+    arch-chroot /mnt /root/after.sh ${disk} usb
+else
+	#this is hard drive
+	echo "install arch on hard drive"
+	sleep 3s
 
-# Chroot into new system
-arch-chroot /mnt /root/after.sh ${disk} #${1}
+	#wipe
+    blkdiscard -f ${disk}
+
+    #partition
+    parted ${disk} --script mklabel msdos
+    parted ${disk} --script mkpart primary linux-swap 1MiB 300MiB #boot /dev/sda1
+    parted ${disk} --script mkpart primary ext4 300MiB 100% #root /dev/sda2
+    parted ${disk} --script set 2 boot on
+    
+    #wipefs
+    wipefs ${disk}1
+    wipefs ${disk}2
+    
+    #mkfs
+    mkfs.ext4 ${disk}2
+    mkswap ${disk}1
+    
+    #mount the partitions
+    mount ${disk}2 /mnt
+    mkdir /mnt/boot
+    swapon ${disk}1
+    
+    #set up time
+    timedatectl set-ntp true
+	    
+    #initate pacman keyring
+    #pacman-key --init
+    #pacman-key --populate archlinux
+    #pacman-key --refresh-keys
+    
+    #pacstrap
+    pacstrap /mnt base linux pacman sudo linux-firmware dosfstools wget
+    
+    #fstab
+    genfstab -U /mnt >> /mnt/etc/fstab
+    
+    #copy post-install system cinfiguration script to new /root
+    curl -k --create-dirs -o /mnt/root/after.sh https://raw.githubusercontent.com/tajo48/ARCH/master/after.sh
+    chmod +x /mnt/root/after.sh
+    
+    #chroot into new system
+    arch-chroot /mnt /root/after.sh ${disk} hard
+fi
