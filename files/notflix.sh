f5eea367f745e1fc70bdf468fff7d4b25c456f10
tajo48 <tajo161626@gmail.com>
2022-01-13 01:01:41 +0100

    update

--- files/notflix.sh
+++ files/notflix.sh
@@ -10,9 +10,9 @@ cachedir="$HOME/.cache/notflix"
 
 
 if [ -z $1 ]; then
-  query=$(xclip -o)
+    query=$(xclip -o)
 else
-  query=$@
+    query=$@
 fi
 
 
@@ -31,39 +31,39 @@ curl -s $baseurl/search/$query/5/ >> $cachedir/tmp.html
 
 # Get Titles
 grep -o '<a href="/torrent/.*</a>' $cachedir/tmp.html |
-  sed 's/<[^>]*>//g' > $cachedir/titles.bw
+sed 's/<[^>]*>//g' > $cachedir/titles.bw
 
 result_count=$(wc -l $cachedir/titles.bw | awk '{print $1}')
 if [ "$result_count" -lt 1 ]; then
-  exit 0
+    exit 0
 fi
 
 # Seeders and Leechers
 grep -o '<td class="coll-2 seeds.*</td>\|<td class="coll-3 leeches.*</td>' $cachedir/tmp.html |
-  sed 's/<[^>]*>//g' | sed 'N;s/\n/ /' > $cachedir/seedleech.bw
+sed 's/<[^>]*>//g' | sed 'N;s/\n/ /' > $cachedir/seedleech.bw
 
 # Size
 grep -o '<td class="coll-4 size.*</td>' $cachedir/tmp.html |
-  sed 's/<span class="seeds">.*<\/span>//g' |
-  sed -e 's/<[^>]*>//g' > $cachedir/size.bw
+sed 's/<span class="seeds">.*<\/span>//g' |
+sed -e 's/<[^>]*>//g' > $cachedir/size.bw
 
 # Links
 grep -E '/torrent/' $cachedir/tmp.html |
-  sed -E 's#.*(/torrent/.*)/">.*/#\1#' |
-  sed 's/td>//g' > $cachedir/links.bw
+sed -E 's#.*(/torrent/.*)/">.*/#\1#' |
+sed 's/td>//g' > $cachedir/links.bw
 
 # Clearning up some data to display
 sed 's/\./ /g; s/\-/ /g' $cachedir/titles.bw |
-  sed 's/[^A-Za-z0-9 ]//g' | tr -s " " > $cachedir/tmp && mv $cachedir/tmp $cachedir/titles.bw
+sed 's/[^A-Za-z0-9 ]//g' | tr -s " " > $cachedir/tmp && mv $cachedir/tmp $cachedir/titles.bw
 
 awk '{print NR " - ["$0"]"}' $cachedir/size.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/size.bw
 awk '{print "[S:"$1 ", L:"$2"]" }' $cachedir/seedleech.bw > $cachedir/tmp && mv $cachedir/tmp $cachedir/seedleech.bw
 
 # Getting the line number
 LINE=$(paste -d\   $cachedir/size.bw $cachedir/seedleech.bw $cachedir/titles.bw |
-  $menu |
-  cut -d\- -f1 |
-  awk '{$1=$1; print}')
+    $menu |
+    cut -d\- -f1 |
+awk '{$1=$1; print}')
 
 
 url=$(head -n $LINE $cachedir/links.bw | tail -n +$LINE)
@@ -71,16 +71,13 @@ fullURL="${baseurl}${url}/"
 
 # Requesting page for magnet link
 curl -s $fullURL > $cachedir/tmp.html
-magnet=$(grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" $cachedir/tmp.html | head -n 1) 
+magnet=$(grep -Po "magnet:\?xt=urn:btih:[a-zA-Z0-9]*" $cachedir/tmp.html | head -n 1)
 
 
-# chosen=$(echo -e "Stream\nGetmagnet" | dmenu -i)
+chosen=$(echo -e "Stream\nGetmagnet\nDownload" | dmenu -i)
 
-# case "$chosen" in
-#   Stream) webtorrent "$magnet" --mpv;;
-#   Getmagnet) echo $magnet | xclip ;;
-#   Download) echo "work in progress" ;;
-# esac
-
-echo $magnet | xclip
-webtorrent "$magnet" --mpv
+case "$chosen" in
+    Stream) webtorrent "$magnet" --mpv;;
+    Getmagnet) echo $magnet | xclip ;;
+    Download) qbittorrent add url "$magnet" ;;
+esac
