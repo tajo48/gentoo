3eb37745257f6345f93955488e1cc1b71945fce5
tajo48 <tajo161626@gmail.com>
2022-04-17 02:32:01 +0200

    update

--- files/github.sh
+++ files/github.sh
@@ -6,7 +6,6 @@ fi
 
 read -p 'Device name: ' device
 device=$(echo $device | sed 's/[^[:alpha:]]//g' | tr '[:upper:]' '[:lower:]' | sed 's/\([a-z]\)\([a-zA-Z0-9]*\)/\u\1\2/g')
-echo "ARCH-"$device"-$(date +"%F")" > /home/tajo48/.ssh/name
 
 ping -q -w 1 -c 1 google.com > /dev/null && echo "internet ok" || exit
 cd /home/tajo48
@@ -15,6 +14,7 @@ git config --global user.email 'tajo161626@gmail.com'
 git config --global user.name 'tajo48'
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -b 4096 -C “tajo161626@gmail.com” -q -N "" -P ""
 
+echo "ARCH-"$device"-$(date +"%F")" > /home/tajo48/.ssh/name
 echo "NAME:"
 cat /home/tajo48/.ssh/name
 echo "KEY:"
