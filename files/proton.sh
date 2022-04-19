fab8136d3711b97c351aa216ab3d658f2f5b69e3
tajo48 <55502906+tajo48@users.noreply.github.com>
2022-04-19 18:06:53 +0200

    update

--- files/proton.sh
+++ files/proton.sh
@@ -2,16 +2,18 @@
 #Xar55 script to update the protonGE
 
 latestGE=$(curl --silent "https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest" | jq -r .tag_name)
-if  [ -d "/home/tajo48/.steam/root/compatibilitytools.d/$latestGE" ]
+if  [ -d "/home/tajo48/.local/share/Steam/compatibilitytools.d/$latestGE" ]
 then
     echo "ProtonGE is up to date"
     exit 1
 else
     echo "ProtonGE is outdated, updating"
-    rm -rf /home/tajo48/.steam/root/compatibilitytools.d/
-    mkdir -p /home/tajo48/.steam/root/compatibilitytools.d/
-    cd /home/tajo48/.steam/root/compatibilitytools.d/
+    rm -rf /home/tajo48/.local/share/Steam/compatibilitytools.d/
+    mkdir -p /home/tajo48/.local/share/Steam/compatibilitytools.d/
+    cd /home/tajo48/.local/share/Steam/compatibilitytools.d/
     wget "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${latestGE}/${latestGE}.tar.gz"
     tar -xvf "${latestGE}.tar.gz"
     rm "${latestGE}.tar.gz"
 fi
+
+
