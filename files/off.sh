f4275b7e33e79f791e7b7622c08df0b4aed1a2ec
tajo48 <tajo161626@gmail.com>
2022-04-20 21:01:51 +0200

    update

--- files/off.sh
+++ files/off.sh
@@ -4,7 +4,7 @@ chosen=$(echo -e "$options" | dmenu -i)
 
 
 case "$chosen" in
-    Xstop) sudo kill -9 $(pgrep -f bar.sh) && sudo kill -9 $(pgrep -f min.sh) &&  sudo killall dwm;;
+    Xstop) sudo kill -9 $(pgrep -f bar.sh) &&  sudo killall dwm;;
     Poweroff) chosen=$(echo -e "now\n-r now" | dmenu -i) && sudo shutdown $chosen ;;
     Virtualbox) sh ~/ARCH/files/vb.sh ;;
 esac
