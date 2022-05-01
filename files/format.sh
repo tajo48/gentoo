3cedc746a5ad6c5ac9a13cd835c27cc0d3269d81
tajo48 <tajo161626@gmail.com>
2022-05-01 02:21:46 +0200

    update

---
+++ files/format.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+if [ -z $1 ]; then
+    selectdisk(){
+        items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
+        options=()
+        IFS_ORIG=$IFS
+        IFS=$'\n'
+        for item in ${items}
+        do
+            options+=("${item}" "")
+        done
+        IFS=$IFS_ORIG
+        result=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
+        if [ "$?" != "0" ]
+        then
+            return 1
+        fi
+        return 0
+    }
+    
+    selectdisk
+    disk=${result%%\ *}
+else
+    disk=$1
+fi
+
+echo "disk: $disk"
+echo "Formatting disk..."
+sleep 6s
+sudo dd if=/dev/zero of=${disk} statuss=progress bs=1M count=1
--- install.sh
+++ install.sh
@@ -1,26 +1,32 @@
 #! /bin/bash
 ping -q -w 1 -c 1 google.com > /dev/null && echo "internet ok" || exit
-selectdisk(){
-    items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
-    options=()
-    IFS_ORIG=$IFS
-    IFS=$'\n'
-    for item in ${items}
-    do
-        options+=("${item}" "")
-    done
-    IFS=$IFS_ORIG
-    result=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
-    if [ "$?" != "0" ]
-    then
-        return 1
-    fi
-    return 0
-}
 
-selectdisk
-disk=${result%%\ *}
+if [ -z $1 ]; then
+    selectdisk(){
+        items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
+        options=()
+        IFS_ORIG=$IFS
+        IFS=$'\n'
+        for item in ${items}
+        do
+            options+=("${item}" "")
+        done
+        IFS=$IFS_ORIG
+        result=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
+        if [ "$?" != "0" ]
+        then
+            return 1
+        fi
+        return 0
+    }
+    
+    selectdisk
+    disk=${result%%\ *}
+else
+    disk=$1
+fi
 usbdrive="..."
+echo "disk: $disk"
 
 for device in /sys/block/*
 do
