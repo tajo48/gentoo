6ef1d18ed3a32d1c04544b26d5f2668bd814d834
tajo48 <tajo161626@gmail.com>
2022-05-02 11:08:40 +0200

    update

---
+++ files/arch.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+ping -q -w 1 -c 1 google.com > /dev/null && echo "internet ok" || exit
+
+if [ -z $1 ]; then
+    selectdisk(){
+        items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
+        options=()
+        IFS_ORIG=$IFS
+        IFS=$'\n'
+        for item in ${items}
+        do
+            options+=("${item}" "")
+        done
+        IFS=$IFS_ORIG
+        result=$(whiptail --backtitle "${APPTITLE}" --title "${1}" --menu "" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
+        if [ "$?" != "0" ]
+        then
+            return 1
+        fi
+        return 0
+    }
+    
+    selectdisk
+    disk=${result%%\ *}
+else
+    disk=$1
+fi
+
+if (whiptail --title "Confirm" --yesno "Are you sure you want to format $disk?" 8 78) then
+    echo "You chose to format $disk"
+else
+    echo "You chose NOT to format $disk"
+    exit
+fi
+
+exit
+
+if (whiptail --title "Turn off when completed" --yesno "Do you want to turn off the computer when the installation is completed $disk?" 8 78) then
+    sudo sh /home/tajo48/ARCH/files/format.sh $disk
+    sudo sh /home/tajo48/ARCH/install.sh $disk
+    sudo shutdown now
+else
+    sudo sh /home/tajo48/ARCH/files/format.sh $disk
+    sudo sh /home/tajo48/ARCH/install.sh $disk
+fi
+
+
--- files/off.sh
+++ files/off.sh
@@ -2,7 +2,6 @@
 options="Poweroff\nXstop\nVirtualbox"
 chosen=$(echo -e "$options" | dmenu -i)
 
-
 case "$chosen" in
     Xstop) sudo kill -9 $(pgrep -f bar.sh) &&  sudo killall dwm;;
     Poweroff) chosen=$(echo -e "now\n-r now" | dmenu -i) && sudo shutdown $chosen ;;
