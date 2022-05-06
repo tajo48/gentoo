74339a5f85a41ebb6d597b9e2f1492f70779c19c
tajo48 <55502906+tajo48@users.noreply.github.com>
2022-05-06 17:39:15 +0200

    Update upgradefiles.sh

--- files/upgradefiles.sh
+++ files/upgradefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/tajo48/
 mkdir -p /home/tajo48/ /home/tajo48/ARCH/
-touch /home/tajo48/.xinitrc /home/tajo48/.zshrc /home/tajo48/ARCH/.git
+touch /home/tajo48/.xinitrc /home/tajo48/.zshrc
 sudo rm -r /home/tajo48/ARCH
 
 ping -q -w 1 -c 1 google.com > /dev/null && echo "internet ok" || exit
@@ -16,20 +16,31 @@ else
     git clone https://github.com/tajo48/ARCH.git
 fi
 
-rm -rf /home/tajo48/.xinitrc /home/tajo48/.zshrc /home/tajo48/ARCH/.git
+rm -rf /home/tajo48/.xinitrc /home/tajo48/.zshrc
 cp /home/tajo48/ARCH/files/xinitrc /home/tajo48/.xinitrc
 cp /home/tajo48/ARCH/files/zshrc /home/tajo48/.zshrc
 
-cd /home/tajo48/Git/ARCH
-
-# if git diff | w is not empty, then exit
-if git diff --quiet; then
-    echo "no changes"
-    sudo rm -r /home/tajo48/Git/ARCH
-    cp -r /home/tajo48/ARCH /home/tajo48/Git/ARCH
+if [ -f "/home/tajo48/Git/ARCH" ]; then
+    cd /home/tajo48/Git/ARCH
+    
+    # if git diff | w is not empty, then exit
+    if git diff --quiet; then
+        echo "no changes"
+        sudo rm -r /home/tajo48/Git/ARCH
+        cp -r /home/tajo48/ARCH /home/tajo48/Git/ARCHc
+    else
+        echo "changes found"
+    fi
 else
-    echo "changes found"
+    cd /home/tajo48/Git
+    if test -f "/home/tajo48/.ssh/id_rsa.pub"; then
+        echo "ssh key exists"
+        git clone git@github.com:tajo48/ARCH.git
+    else
+        echo "ssh key not found"
+        git clone https://github.com/tajo48/ARCH.git
+    fi
+    
 fi
 
-
-
+rm -rf /home/tajo48/ARCH/.git
