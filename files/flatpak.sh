cc1ce5e6972f35127c51e8a6d8c14b1cd1de52c1
tajo48 <tajo161626@gmail.com>
2022-07-07 15:55:17 +0200

    update

---
+++ files/flatpak.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install --assumeyes --noninteractive flathub org.gimp.GIMP
+flatpak install --assumeyes --noninteractive com.discordapp.Discord
+flatpak install --assumeyes --noninteractive org.blender.Blender
+flatpak install --assumeyes --noninteractive com.github.Anuken.Mindustry
+flatpak install --assumeyes --noninteractive io.bassi.Amberol
+flatpak install --assumeyes --noninteractive com.belmoussaoui.Authenticator
+flatpak install --assumeyes --noninteractive com.obsproject.Studio
+flatpak install --assumeyes --noninteractive com.gitlab.guillermop.MasterKey
+flatpak install --assumeyes --noninteractive info.febvre.Komikku
+flatpak install --assumeyes --noninteractive io.github.mpobaschnig.Vaults
+flatpak install --assumeyes --noninteractive org.gabmus.whatip
+flatpak install --assumeyes --noninteractive org.gnome.Solanum
+flatpak install --assumeyes --noninteractive sh.ppy.osu
+flatpak install --assumeyes --noninteractive com.github.johnfactotum.QuickLookup
+flatpak install --assumeyes --noninteractive org.gnome.World.PikaBackup
