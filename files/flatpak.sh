a56042505a9b674ecf68e7e0afbca62d5f99746d
tajo48 <tajo161626@gmail.com>
2022-07-09 20:20:49 +0200

    update

--- files/flatpak.sh
+++ files/flatpak.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install --assumeyes --noninteractive flathub org.gimp.GIMP
-flatpak install --assumeyes --noninteractive com.discordapp.Discord
-flatpak install --assumeyes --noninteractive org.blender.Blender
-flatpak install --assumeyes --noninteractive com.github.Anuken.Mindustry
-flatpak install --assumeyes --noninteractive io.bassi.Amberol
-flatpak install --assumeyes --noninteractive com.belmoussaoui.Authenticator
-flatpak install --assumeyes --noninteractive com.obsproject.Studio
-flatpak install --assumeyes --noninteractive com.gitlab.guillermop.MasterKey
-flatpak install --assumeyes --noninteractive info.febvre.Komikku
-flatpak install --assumeyes --noninteractive io.github.mpobaschnig.Vaults
-flatpak install --assumeyes --noninteractive org.gabmus.whatip
-flatpak install --assumeyes --noninteractive org.gnome.Solanum
-flatpak install --assumeyes --noninteractive sh.ppy.osu
-flatpak install --assumeyes --noninteractive com.github.johnfactotum.QuickLookup
-flatpak install --assumeyes --noninteractive org.gnome.World.PikaBackup
+flatpak install --assumeyes --noninteractive flathub flathub org.gimp.GIMP
+flatpak install --assumeyes --noninteractive flathub com.discordapp.Discord
+flatpak install --assumeyes --noninteractive flathub org.blender.Blender
+flatpak install --assumeyes --noninteractive flathub com.github.Anuken.Mindustry
+flatpak install --assumeyes --noninteractive flathub io.bassi.Amberol
+flatpak install --assumeyes --noninteractive flathub com.belmoussaoui.Authenticator
+flatpak install --assumeyes --noninteractive flathub com.obsproject.Studio
+flatpak install --assumeyes --noninteractive flathub com.gitlab.guillermop.MasterKey
+flatpak install --assumeyes --noninteractive flathub info.febvre.Komikku
+flatpak install --assumeyes --noninteractive flathub io.github.mpobaschnig.Vaults
+flatpak install --assumeyes --noninteractive flathub org.gabmus.whatip
+flatpak install --assumeyes --noninteractive flathub org.gnome.Solanum
+flatpak install --assumeyes --noninteractive flathub sh.ppy.osu
+flatpak install --assumeyes --noninteractive flathub com.github.johnfactotum.QuickLookup
+flatpak install --assumeyes --noninteractive flathub org.gnome.World.PikaBackup
+flatpak install --assumeyes --noninteractive flathub com.mattjakeman.ExtensionManager
