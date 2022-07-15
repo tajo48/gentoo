3bcde52d23b978352e125a7a7ea9477106d31599
tajo48 <tajo161626@gmail.com>
2022-07-16 00:39:44 +0200

    update

--- files/flatpak.sh
+++ files/flatpak.sh
@@ -20,7 +20,7 @@ flatpak install --assumeyes --noninteractive flathub app.drey.Warp
 flatpak install --assumeyes --noninteractive flathub org.gabmus.gfeeds
 flatpak install --assumeyes --noninteractive flathub org.onlyoffice.desktopeditors
 flatpak install --assumeyes --noninteractive flathub app.drey.Dialect
-#flatpak install --assumeyes --noninteractive flathub
+flatpak install --assumeyes --noninteractive flathub org.gnome.Firmware
 #flatpak install --assumeyes --noninteractive flathub
 #flatpak install --assumeyes --noninteractive flathub
 #flatpak install --assumeyes --noninteractive flathub
