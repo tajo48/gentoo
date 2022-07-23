b7ef608734c54761d3132e597cdc3de0c3f38cc2
tajo48 <55502906+tajo48@users.noreply.github.com>
2022-07-23 12:39:30 +0200

    Update youtube.sh

--- files/youtube.sh
+++ files/youtube.sh
@@ -16,5 +16,5 @@ fi
 cat $@ | while read line || [[ -n $line ]];
 do
     echo $line
-    yt-dlp  $line -x --embed-thumbnail --embed-metadata --restrict-filenames
+    yt-dlp  $line -x --embed-thumbnail --embed-metadata --restrict-filenames --convert-thumbnails png
 done
