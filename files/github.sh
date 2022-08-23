5b06b7a2a3a869331d71ff4eca8f487de1dbc12b
tajo48 <55502906+tajo48@users.noreply.github.com>
2022-08-24 00:02:22 +0200

    Update github.sh

--- files/github.sh
+++ files/github.sh
@@ -5,13 +5,15 @@ if test -f "/home/tajo48/.ssh/id_rsa.pub"; then
 fi
 
 read -p 'Device name: ' device
+read -p 'Your email: ' email
+read -p 'Github username: ' name
 device=$(echo $device | sed 's/[^[:alpha:]]//g' | tr '[:upper:]' '[:lower:]' | sed 's/\([a-z]\)\([a-zA-Z0-9]*\)/\u\1\2/g')
 
 ping -q -w 1 -c 1 google.com > /dev/null && echo "internet ok" || exit
 cd /home/tajo48
 git config --global credential.helper store
-git config --global user.email 'tajo161626@gmail.com'
-git config --global user.name 'tajo48'
+git config --global user.email $email
+git config --global user.name $name
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -b 4096 -C “tajo161626@gmail.com” -q -N "" -P ""
 
 echo "ARCH-"$device"-$(date +"%F")" > /home/tajo48/.ssh/name
