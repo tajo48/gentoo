0c97b7aaff885c24ee0f5c743be13374790145e4
tajo48 <55502906+tajo48@users.noreply.github.com>
2022-10-30 22:26:14 +0100

    Update flatpak.sh

--- files/flatpak.sh
+++ files/flatpak.sh
@@ -18,40 +18,49 @@ flatpak install --assumeyes --noninteractive --user flathub com.github.johnfacto
 flatpak install --assumeyes --noninteractive --user flathub org.gnome.World.PikaBackup
 flatpak install --assumeyes --noninteractive --user flathub com.mattjakeman.ExtensionManager
 flatpak install --assumeyes --noninteractive --user flathub app.drey.Warp
-#flatpak install --assumeyes --noninteractive --user flathub org.gabmus.gfeeds <-dont work 100%
 flatpak install --assumeyes --noninteractive --user flathub org.onlyoffice.desktopeditors
-flatpak install --assumeyes --noninteractive --user flathub app.drey.Dialect
 flatpak install --assumeyes --noninteractive --user flathub org.gnome.Firmware
 flatpak install --assumeyes --noninteractive --user flathub com.lakoliu.Furtherance
 flatpak install --assumeyes --noninteractive --user flathub com.belmoussaoui.Decoder
 flatpak install --assumeyes --noninteractive --user flathub com.github.tchx84.Flatseal
 flatpak install --assumeyes --noninteractive --user flathub com.github.tenderowl.frog
-flatpak install --assumeyes --noninteractive --user flathub io.posidon.Paper
 flatpak install --assumeyes --noninteractive --user flathub com.usebottles.bottles
 flatpak install --assumeyes --noninteractive --user flathub com.github.ADBeveridge.Raider
-#flatpak install --assumeyes --noninteractive --user flathub dev.Cogitri.Health <-crash table flip
 flatpak install --assumeyes --noninteractive --user flathub org.gnome.SoundRecorder
 flatpak install --assumeyes --noninteractive --user flathub com.github.GradienceTeam.Gradience
 flatpak install --assumeyes --noninteractive --user flathub pm.mirko.Atoms
-#flatpak install --assumeyes --noninteractive --user flathub app.drey.EarTag <- does not open opus???
-#flatpak install --assumeyes --noninteractive --user flathub de.haeckerfelix.Fragments <- give it second chance
-#flatpak install --assumeyes --noninteractive --user flathub-beta com.github.marhkb.Pods <- dockermoment
+flatpak install --assumeyes --noninteractive --user flathub de.haeckerfelix.Fragments
+
+
+#school
 flatpak install --assumeyes --noninteractive --user flathub com.github.alexhuntley.Plots
+flatpak install --assumeyes --noninteractive --user flathub io.posidon.Paper
+flatpak install --assumeyes --noninteractive --user flathub app.drey.Dialect
+flatpak install --assumeyes --noninteractive --user flathub com.github.fushinari.Wordbook
+flatpak install --assumeyes --noninteractive --user flathub com.github.flxzt.rnote
+
+#dev tools
+#flatpak install --assumeyes --noninteractive --user flathub org.gnome.Glade <- gtk
+#flatpak install --assumeyes --noninteractive --user flathub-beta com.github.marhkb.Pods <- dockermoment
 flatpak install --assumeyes --noninteractive --user flathub com.github.finefindus.eyedropper
-#flatpak install --assumeyes --noninteractive --user flathub org.gnome.Glade <- wait
+#flatpak install --assumeyes --noninteractive --user flathub io.github.achetagames.epic_asset_manager <-crash table flip
+
+#flatpak install --assumeyes --noninteractive --user flathub org.gabmus.gfeeds <-dont work 100%
+#flatpak install --assumeyes --noninteractive --user flathub dev.Cogitri.Health <-crash table flip
+#flatpak install --assumeyes --noninteractive --user flathub app.drey.EarTag <- does not open opus???
+#flatpak install --assumeyes --noninteractive --user flathub org.gnome.Fractal <- gtk matrix
+#flatpak install --assumeyes --noninteractive --user flathub com.dec05eba.gpu_screen_recorder <- check out if this shit works on every gpu (no it doesn't)
+#flatpak install --assumeyes --noninteractive --user flathub nl.g4d.Girens <- Plex GTK client
+#flatpak install --assumeyes --noninteractive --user flathub fr.romainvigier.zap <- soundboard
 #https://gitlab.gnome.org/sophie-h/key-rack <- wait for flathub release
-#https://github.com/fushinari/Wordbook
-#https://gitlab.com/rmnvgr/zap
-#https://gitlab.gnome.org/GNOME/fractal
-#flatpak install flathub com.dec05eba.gpu_screen_recorder <- check out if this shit works on every gpu (no it doesn't)
-#https://flathub.org/apps/details/nl.g4d.Girens
+
 #games
 flatpak install --assumeyes --noninteractive --user flathub sh.ppy.osu
 flatpak install --assumeyes --noninteractive launcher.moe com.gitlab.KRypt0n_.an-anime-game-launcher
 flatpak install --assumeyes --noninteractive --user flathub com.heroicgameslauncher.hgl
-#flatpak install --assumeyes --noninteractive --user flathub io.github.achetagames.epic_asset_manager <-crash table flip
+flatpak install --assumeyes --noninteractive --user flathub org.prismlauncher.PrismLauncher
 flatpak install --assumeyes --noninteractive --user flathub com.github.Anuken.Mindustry
-#flatpak install --assumeyes --noninteractive --user flathub io.github.Foldex.AdwSteamGtk
+flatpak install --assumeyes --noninteractive --user flathub io.github.Foldex.AdwSteamGtk
 
 #config
 sudo flatpak override com.usebottles.bottles --filesystem=xdg-data/applications
